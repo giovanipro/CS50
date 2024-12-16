e8019f15eead02e502d9079bf39fd227d21ea44e
Giovani <giovanipro@outlook.com.br>
2024-12-16 15:39:35 -0300

    Mario Challenge Added

---
+++ mario/Mario.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        printf("Type the height: ");
+        scanf("%i", &n);
+    } while (n < 1 || n > 8);   
+    
+    for(int j = 0; j < n; j++)
+    {
+        for(int i = 0; i < n; i++)  
+        {
+            if(n - j - 2 < i)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
